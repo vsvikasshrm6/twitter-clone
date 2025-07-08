88a476135cdb4eb10ca6c8431f2bb6a5aeb3511a
vsvikashrm6 <vsvikasshrm6@gmail.com>
2025-07-08 23:05:34 +0530

    DiasyUitobeIntegrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route} from "react-router-dom"
+import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+import SignupPage from './Pages/SignupPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return (<div>
+  <Routes>
+    <Route path='/' element= {<HomePage></HomePage>}></Route>
+    <Route path='/login' element= {<LoginPage></LoginPage>}></Route>
+    <Route path='/signup' element= {<SignupPage></SignupPage>}></Route>
+  </Routes>
+  </div>)
 }
 
 export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui,],
+  daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
+}
