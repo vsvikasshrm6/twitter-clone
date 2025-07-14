c457381d03b2d69ee59e16015d1a9a7801059b42
Sharma <vsvikasshrm6@gmail.com>
2025-07-14 16:07:39 +0530

    UIdesigndone

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -3,20 +3,28 @@ import { Link } from "react-router-dom";
 
 import Posts from "../components/common/Posts";
 
-import EditProfileModal from "../components/common/EditProfileModal"
+import EditProfileModal from "../components/common/EditProfileModal";
 
-import { POSTS } from "../components/utils/db/dummy"
+import { POSTS } from "../components/utils/db/dummy";
 
 import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
+import ProfileHeaderSkeleton from "../components/skeletons/ProfileHeaderSkeleton"
 
 const ProfilePage = () => {
-  const coverImgRef = useRef(null);
+	const [coverImg, setCoverImg] = useState(null);
+	const [profileImg, setProfileImg] = useState(null);
+	const [feedType, setFeedType] = useState("posts");
+
+	const coverImgRef = useRef(null);
 	const profileImgRef = useRef(null);
 
-  const user = {
+	const isLoading = false;
+	const isMyProfile = true;
+
+	const user = {
 		_id: "1",
 		fullName: "John Doe",
 		username: "johndoe",
@@ -28,32 +36,165 @@ const ProfilePage = () => {
 		followers: ["1", "2", "3"],
 	};
 
-  return (
-    <div className="w-full flex flex-col">
-      <div className="flex p-4 gap-8 items-center">
-        <Link><FaArrowLeft></FaArrowLeft></Link>
-        <div>
-          <h1 className="font-bold">{user.fullName}</h1>
-          <span className="text-gray-500">{POSTS.length} Post</span>
-        </div>
-      </div>
-      <div className="w-full relative">
-        <img src={user.coverImg} alt="" className="w-full h-56"/>
-        <input type="file" ref={coverImgRef} className="hidden"/>
-        <button className="flex justify-center items-center absolute top-4 right-4 rounded-full size-9 bg-slate-900" onClick={()=>coverImgRef.current.click()}><MdEdit className="size-6"></MdEdit></button>
-      </div>
-      <div className="flex items-center relative pt-10">
-        <div className="absolute -top-20 left-4">
-          <div className="relative rounded-full">
-            <button onClick={profileImgRef.current.click() } className="absolute top-0 right-0"><MdEdit className="size-4"></MdEdit></button>
-            <input type="file" className="hidden" ref={profileImgRef}/>
-            <img src={user.profileImg} className="w-48 rounded-full" /> 
-          </div>
-          </div>
-        <button className="btn ml-auto border border-gray-400 rounded-full">Edit Profile</button>
-      </div>
-    </div>
-  )
-}
+	const handleImgChange = (e, state) => {
+		const file = e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onload = () => {
+				state === "coverImg" && setCoverImg(reader.result);
+				state === "profileImg" && setProfileImg(reader.result);
+			};
+			reader.readAsDataURL(file);
+		}
+	};
 
-export default ProfilePage
+	return (
+		<>
+			<div className='flex-[4_4_0]  border-r border-gray-700 min-h-screen '>
+				{/* HEADER */}
+				{isLoading && <ProfileHeaderSkeleton />}
+				{!isLoading && !user && <p className='text-center text-lg mt-4'>User not found</p>}
+				<div className='flex flex-col'>
+					{!isLoading && user && (
+						<>
+							<div className='flex gap-10 px-4 py-2 items-center'>
+								<Link to='/'>
+									<FaArrowLeft className='w-4 h-4' />
+								</Link>
+								<div className='flex flex-col'>
+									<p className='font-bold text-lg'>{user?.fullName}</p>
+									<span className='text-sm text-slate-500'>{POSTS?.length} posts</span>
+								</div>
+							</div>
+							{/* COVER IMG */}
+							<div className='relative group/cover'>
+								<img
+									src={coverImg || user?.coverImg || "/cover.png"}
+									className='h-52 w-full object-cover'
+									alt='cover image'
+								/>
+								{isMyProfile && (
+									<div
+										className='absolute top-2 right-2 rounded-full p-2 bg-gray-800 bg-opacity-75 cursor-pointer opacity-0 group-hover/cover:opacity-100 transition duration-200'
+										onClick={() => coverImgRef.current.click()}
+									>
+										<MdEdit className='w-5 h-5 text-white' />
+									</div>
+								)}
+
+								<input
+									type='file'
+									hidden
+									ref={coverImgRef}
+									onChange={(e) => handleImgChange(e, "coverImg")}
+								/>
+								<input
+									type='file'
+									hidden
+									ref={profileImgRef}
+									onChange={(e) => handleImgChange(e, "profileImg")}
+								/>
+								{/* USER AVATAR */}
+								<div className='avatar absolute -bottom-16 left-4'>
+									<div className='w-32 rounded-full relative group/avatar'>
+										<img src={profileImg || user?.profileImg || "/avatar-placeholder.png"} />
+										<div className='absolute top-5 right-3 p-1 bg-primary rounded-full group-hover/avatar:opacity-100 opacity-0 cursor-pointer'>
+											{isMyProfile && (
+												<MdEdit
+													className='w-4 h-4 text-white'
+													onClick={() => profileImgRef.current.click()}
+												/>
+											)}
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className='flex justify-end px-4 mt-5'>
+								{isMyProfile && <EditProfileModal />}
+								{!isMyProfile && (
+									<button
+										className='btn btn-outline rounded-full btn-sm'
+										onClick={() => alert("Followed successfully")}
+									>
+										Follow
+									</button>
+								)}
+								{(coverImg || profileImg) && (
+									<button
+										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
+										onClick={() => alert("Profile updated successfully")}
+									>
+										Update
+									</button>
+								)}
+							</div>
+
+							<div className='flex flex-col gap-4 mt-14 px-4'>
+								<div className='flex flex-col'>
+									<span className='font-bold text-lg'>{user?.fullName}</span>
+									<span className='text-sm text-slate-500'>@{user?.username}</span>
+									<span className='text-sm my-1'>{user?.bio}</span>
+								</div>
+
+								<div className='flex gap-2 flex-wrap'>
+									{user?.link && (
+										<div className='flex gap-1 items-center '>
+											<>
+												<FaLink className='w-3 h-3 text-slate-500' />
+												<a
+													href='https://youtube.com/@asaprogrammer_'
+													target='_blank'
+													rel='noreferrer'
+													className='text-sm text-blue-500 hover:underline'
+												>
+													youtube.com/@asaprogrammer_
+												</a>
+											</>
+										</div>
+									)}
+									<div className='flex gap-2 items-center'>
+										<IoCalendarOutline className='w-4 h-4 text-slate-500' />
+										<span className='text-sm text-slate-500'>Joined July 2021</span>
+									</div>
+								</div>
+								<div className='flex gap-2'>
+									<div className='flex gap-1 items-center'>
+										<span className='font-bold text-xs'>{user?.following.length}</span>
+										<span className='text-slate-500 text-xs'>Following</span>
+									</div>
+									<div className='flex gap-1 items-center'>
+										<span className='font-bold text-xs'>{user?.followers.length}</span>
+										<span className='text-slate-500 text-xs'>Followers</span>
+									</div>
+								</div>
+							</div>
+							<div className='flex w-full border-b border-gray-700 mt-4'>
+								<div
+									className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 relative cursor-pointer'
+									onClick={() => setFeedType("posts")}
+								>
+									Posts
+									{feedType === "posts" && (
+										<div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary' />
+									)}
+								</div>
+								<div
+									className='flex justify-center flex-1 p-3 text-slate-500 hover:bg-secondary transition duration-300 relative cursor-pointer'
+									onClick={() => setFeedType("likes")}
+								>
+									Likes
+									{feedType === "likes" && (
+										<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary' />
+									)}
+								</div>
+							</div>
+						</>
+					)}
+
+					<Posts />
+				</div>
+			</div>
+		</>
+	);
+};
+export default ProfilePage;
